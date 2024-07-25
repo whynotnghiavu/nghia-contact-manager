7d709cefc272744fc36835fa797177c35200ad53
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 15:11:14 +0700

    update app.py and contents/templates/index.html

--- contents/app.py
+++ contents/app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
-    with open("data/database.txt", "r") as file:
-        vcards = [line.strip() for line in file.readlines()]
+def index():
+# def home():
+#     with open("data/database.txt", "r") as file:
+#         vcards = [line.strip() for line in file.readlines()]
 
+#     return render_template('index.html', vcards=vcards)
+    vcards = [
+        {'name': 'John Doe', 'email': 'john@example.com'},
+        {'name': 'Jane Smith', 'email': 'jane@example.com'},
+        {'name': 'Emily Johnson', 'email': 'emily@example.com'}
+    ]
+    
     return render_template('index.html', vcards=vcards)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
-
