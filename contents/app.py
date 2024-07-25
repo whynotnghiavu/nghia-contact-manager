b2c5142f075edaa8fe48dc6cf281fa780e424fe4
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 18:54:14 +0700

    update app.py and add.html

--- contents/app.py
+++ contents/app.py
@@ -3,9 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    with open("data/database.txt", "r") as file:
+def get_all_vcards_from_database(txt_file_path):
+    with open(txt_file_path, "r") as file:
         contents = file.readlines()
     vcards = []
 
@@ -15,6 +14,13 @@ def index():
         vcard = {'name': name, 'phone': phone}
         vcards.append(vcard)
 
+    return vcards
+
+
+@app.route('/')
+def index():
+    vcards = get_all_vcards_from_database("data/database.txt")
+
     return render_template('index.html', vcards=vcards)
 
 
@@ -23,13 +29,14 @@ def add():
     return render_template('add.html')
 
 
-# @app.route('/add', methods=['POST'])
-# def add():
-#     # phone = request.form.get('phone')
-#     # name = request.form.get('name')
-#     # if phone and name:
+@app.route('/api/add', methods=['POST'])
+def api_add():
+    phone = request.form.get('phone')
+    name = request.form.get('name')
 #     #     vcards.append({'phone': phone, 'name': name})
-#     return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+
 # # Các route khác như chỉnh sửa, xóa có thể được thêm vào đây
 if __name__ == '__main__':
     app.run(debug=True)
