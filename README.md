ad328851d9559b49ec2c3c3a7d9099914f57947d
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 19:04:51 +0700

    update README.md and app.py

--- README.md
+++ README.md
@@ -25,16 +25,15 @@ tên giống thì bỏ qua
 tên k giống thì hỏi chọn 1 hay 2?
 
 
-
-Bạn đã gửi
-Todo List
-Bạn đã gửi
-Tên giống thì +?
-Bạn đã gửi
-Trước khi lưu phải sort
-Bạn đã gửi
+  
+ 
+ 
 Nếu trùng tạo file và trang Trùng lặp
-Bạn đã gửi
-Chưa được thêm
-Bạn đã gửi
+  
+ 
 Trùng lặp số điện thoại
+<!--  -->
+Trước khi lưu phải sort
+
+<!--  -->
+sửa cũng cần kiểm tra trùng lặp
--- contents/app.py
+++ contents/app.py
@@ -16,7 +16,6 @@ def get_all_vcards_from_database(txt_file_path):
 
     return vcards
 
-
 @app.route('/')
 def index():
     vcards = get_all_vcards_from_database("data/database.txt")
@@ -29,29 +28,48 @@ def add():
     return render_template('add.html')
 
 
-@app.route('/api/add', methods=['POST'])
-def api_add():
-    phone = request.form.get('phone')
-    name = request.form.get('name')
+# @app.route('/api/add', methods=['POST'])
+# def api_add():
+#     phone = request.form.get('phone')
+#     name = request.form.get('name')
 
-    vcards = get_all_vcards_from_database("data/database.txt")
+#     vcards = get_all_vcards_from_database("data/database.txt")
 
     
-    for vcard in vcards:
-        print(vcard["phone"])
-        print(vcard["name"])
+#     # for vcard in vcards:
+#     #     print(vcard["phone"])
+#     #     print(vcard["name"])
 
-        # if phone!= vcard["phone"]: thêm dữ liệu mới
-        # if phone= vcard["phone"]: xét name
-        #     if name = vcard["name"]: bỏ qua không làm gì cả
-        #     if name != vcard["name"]: ghi vào file error.txt
+#     #     # if phone!= vcard["phone"]: thêm dữ liệu mới
+#     #     # if phone= vcard["phone"]: xét name
+#     #     #     if name = vcard["name"]: bỏ qua không làm gì cả
+#     #     #     if name != vcard["name"]: ghi vào file error.txt
         
  
 
 
-    return redirect(url_for('index'))
+#     # return redirect(url_for('index'))
 
+#     phone_exists = False
+
+#     for vcard in vcards:
+#         if phone == vcard["phone"]:
+#             phone_exists = True
+#             if name != vcard["name"]:
+#                 write_error_to_file("data/error.txt", phone, name)
+#             break
+
+#     if not phone_exists:
+#         write_vcard_to_database("data/database.txt", phone, name)
+
+#     return redirect(url_for('index'))
 
 # # Các route khác như chỉnh sửa, xóa có thể được thêm vào đây
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# take note
+# readwrite
+# read
