b76d907032aba6ffd408f4ab9f2ce43947869a92
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 12:45:41 +0700

    update app.py

--- contents/app.py
+++ contents/app.py
@@ -1,14 +1,16 @@
 # pip install flask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     with open("phone_numbers.txt", "r") as file:
-        content = file.read()
-    
-    return f"<pre>{content}</pre>"
+        contents = file.read()
+
+    return render_template('index.html', content=contents)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
