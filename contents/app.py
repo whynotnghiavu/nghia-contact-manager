754d8c2b5a9c00784aeaab8668c9e9e9624ece8f
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 12:49:32 +0700

    update app.py and contents/templates/index.html

--- contents/app.py
+++ contents/app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     with open("phone_numbers.txt", "r") as file:
-        contents = file.read()
+        # contents = file.read()
+        phone_numbers = [line.strip() for line in file.readlines()]
 
-    return render_template('index.html', content=contents)
+    return render_template('index.html', phone_numbers=phone_numbers)
 
 
 if __name__ == '__main__':
