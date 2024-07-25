d99f6ebc4c8ba7e7e7aa54b0a2f7785a546a6058
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 23:07:00 +0700

    delete 1 file and update 1 file

--- contents/export.py
+++ contents/export.py
@@ -1,14 +1,15 @@
 import quopri
 
+
 def read_txt(txt_file_path):
     with open(txt_file_path, 'r', encoding='utf-8') as txt_file:
         contents = txt_file.readlines()
 
     vcards = []
-    
+
     for content in contents:
         content = content.strip()
-        if content:  # Ensure the line is not empty
+        if content:
             phone, name = content.split(',')
             vcard = {
                 'name': name,
@@ -18,16 +19,23 @@ def read_txt(txt_file_path):
 
     return vcards
 
+
 def write_vcf(vcards, vcf_file_path):
     with open(vcf_file_path, 'w', encoding='utf-8') as vcf_file:
         for vcard in vcards:
+            name = vcard.get("name", "")
+            phone = vcard.get("phone", "")
+
+            # Encode name in quoted-printable
+            encoded_name = quopri.encodestring(name.encode('utf-8')).decode('utf-8').replace('\n', '')
+
             vcf_file.write('BEGIN:VCARD\n')
             vcf_file.write('VERSION:2.1\n')
-            vcf_file.write(f'FN:{quopri.encodestring(vcard["name"].encode("utf-8")).decode("utf-8")}\n')
-# FN;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:=4D=E1=BA=B9
-            vcf_file.write(f'TEL;CELL;PREF:{vcard["phone"]}\n')
+            vcf_file.write(f'FN;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:{encoded_name}\n')
+            vcf_file.write(f'TEL;CELL;PREF:{phone}\n')
             vcf_file.write('END:VCARD\n')
-            vcf_file.write('\n')  # Add a blank line between vCards for readability
+            vcf_file.write('\n')
+
 
 def export():
     txt_file_path = "data/database.txt"
@@ -35,6 +43,7 @@ def export():
 
     vcards = read_txt(txt_file_path)
     write_vcf(vcards, vcf_file_path)
+
 
 if __name__ == '__main__':
     export()
