4fa080ff902b73b7d659e31c7c0c4478d19f6d71
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 22:01:18 +0700

    update script.js

--- contents/static/script.js
+++ contents/static/script.js
@@ -1,9 +1,7 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
   const selectAllCheckbox = document.getElementById("selectAll");
   const rowCheckboxes = document.querySelectorAll(".selectRow");
 
-  // Khi chọn tất cả
   selectAllCheckbox.addEventListener("change", (event) => {
     rowCheckboxes.forEach((checkbox) => {
       checkbox.checked = event.target.checked;
@@ -11,12 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // Khi chọn các hàng riêng lẻ
   rowCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
       checkbox.closest("tr").classList.toggle("selected", checkbox.checked);
 
-      // Cập nhật trạng thái của ô chọn tất cả
       const allChecked = Array.from(rowCheckboxes).every((cb) => cb.checked);
       selectAllCheckbox.checked = allChecked;
     });
@@ -31,10 +27,6 @@ function deletePhoneNumber(phone) {
   if (confirm("Bạn có chắc chắn muốn xóa số điện thoại này không?")) {
     fetch(`/delete/${phone}`, {
       method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //     "X-CSRFToken": "{{ csrf_token() }}", // Nếu bạn sử dụng CSRF protection
-    //   },
     }).then((response) => {
       if (response.ok) {
         window.location.reload();
